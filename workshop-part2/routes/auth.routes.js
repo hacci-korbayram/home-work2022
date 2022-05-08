e25908b93250f7aee49246503e5972e23fd5038c
hacci <hacci_mk@hotmail.com>
2022-05-08 23:56:52 +0200

    delete worksop-part2

--- workshop-part2/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const AuthController = require("../controllers/auth.controller");
-
-
-router.post("/login", AuthController.loginUser);
-
-module.exports = router;
